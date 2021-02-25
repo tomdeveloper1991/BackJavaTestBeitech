6f42ff97e31b3407e3ffaa0b6c5f7827ee52b9d6
Tomas Lopez <david.lopez.1999@gmail.com>
2021-02-25 17:16:36 -0500

    Se realiza el primer push del proyecto para sincronizarlo con github

---
+++ BackJavaTestBeitech/src/main/java/com/beitech/order/BackJavaTestBeitechApplication.java
@@ -0,0 +1,13 @@
+package com.beitech.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackJavaTestBeitechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackJavaTestBeitechApplication.class, args);
+	}
+
+}
---
+++ BackJavaTestBeitech/src/test/java/com/beitech/order/BackJavaTestBeitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beitech.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackJavaTestBeitechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
