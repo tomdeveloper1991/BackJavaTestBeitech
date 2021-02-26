9f9b00fa3ce0c56d64e54e80b12562162cb0123b
Tomas Lopez <david.lopez.1999@gmail.com>
2021-02-26 10:33:27 -0500

    Se agrega Service, Dao y Modelo para mas entidades

--- BackJavaTestBeitech/src/main/java/com/beitech/order/api/OrderApi.java
+++ BackJavaTestBeitech/src/main/java/com/beitech/order/api/OrderApi.java
@@ -9,22 +9,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.beitech.order.models.Customer;
 import com.beitech.order.models.Order;
+import com.beitech.order.models.Product;
+import com.beitech.order.services.CustomerService;
 import com.beitech.order.services.OrderService;
+import com.beitech.order.services.ProductService;
 
 @RestController
 public class OrderApi {
 		
 	@Autowired
-	private OrderService service;		
+	private OrderService orderService;
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@Autowired
+	private ProductService productService;
 	
 	@RequestMapping(value="/getOrdersByCustomerAndDate/{customerId}/{fechaInicial}/{fechaFinal}",method = RequestMethod.GET)
 	public List<Order> getAllOrders(@PathVariable(value="customerId") Integer customerId,
 									@PathVariable(value="fechaInicial") String fechaInicial,
 									@PathVariable(value="fechaFinal") String fechaFinal){
 		List<Order> result = new ArrayList<Order>();
-		System.out.println(customerId);
-		result=service.getAllOrdersByClientAndDate(customerId, fechaInicial, fechaFinal);
+		List<Customer> result2 = new ArrayList<Customer>();
+		List<Product> result3 = new ArrayList<Product>();
+		
+		result=orderService.getAllOrdersByClientAndDate(customerId, fechaInicial, fechaFinal);
+		
+		result2=customerService.findAll();
+		
+		for (Customer customer : result2) {
+			System.out.println(customer.getCustomerId()+" - "+customer.getName()+" - "+customer.getEmail());
+		}
+		
+		result3=productService.findAll();
+		
+		for (Product product : result3) {
+			System.out.println(product.getProductId()+" - "
+								+product.getName()+" - "
+								+product.getProductDescription()+" - "
+								+product.getPrice());
+		}
 		
 		return result;
 	}
