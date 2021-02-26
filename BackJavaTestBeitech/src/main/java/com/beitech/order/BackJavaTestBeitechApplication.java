0326f2a239f437df7d5f018a28c0f958dd019aa3
Tomas Lopez <david.lopez.1999@gmail.com>
2021-02-25 21:37:22 -0500

    Se hace commit de primer prototipo de api

--- BackJavaTestBeitech/src/main/java/com/beitech/order/BackJavaTestBeitechApplication.java
+++ BackJavaTestBeitech/src/main/java/com/beitech/order/BackJavaTestBeitechApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackJavaTestBeitechApplication {
+public class BackJavaTestBeitechApplication {		
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackJavaTestBeitechApplication.class, args);
